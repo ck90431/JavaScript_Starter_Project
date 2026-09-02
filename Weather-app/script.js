bb9cf922e1ae344fd74349d4361100d903967816
ck90431 <ck90431@gmail.com>
2026-09-02 18:38:55 +0530

    link

--- Weather-app/script.js
+++ Weather-app/script.js
@@ -52,33 +52,3 @@ searchBtn.addEventListener("click", () => {
 });
 
 
-
-
-// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
-// const searchBox = document.querySelector(".search-box input");
-// const searchBtn = document.querySelector(".search-box button");
-
-// async function weatherCheck(city) {
-
-//     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
-//     const data = await response.json();
-
-//     console.log(data);
-
-//     document.querySelector(".city").innerHTML = data.name;
-
-//     document.querySelector(".temp").innerHTML =
-//         Math.round(data.main.temp) + "°C";
-
-//     document.querySelector(".humidity").innerHTML =
-//         data.main.humidity + "%";
-
-//     document.querySelector(".wind").innerHTML =
-//         data.wind.speed + " km/hr";
-// }
-
-// searchBtn.addEventListener("click", () => {
-//     weatherCheck(searchBox.value);
-// });
